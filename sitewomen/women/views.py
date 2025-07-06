ee8c0f9a67dd5b35d3138d7981c8af824969746d
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-07-06 17:17:37 +0300

    применение тегов

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,23 +5,17 @@ from django.template.loader import render_to_string
 
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулиа Робертс', 'content': 'Биография Джулиа Робертс', 'is_published': True},
+]
 
 def index(request):
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
-
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'float': 28.56,
-        'lst': [1, 2, 'abc', True],
-        'set': {1, 2, 3, 4, 5},
-        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-        'obj': MyClass(10, 20),
+        'posts': data_db,
     }
     return render(request, 'women/index.html', context=data)
 
