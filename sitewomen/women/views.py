377971f2f6476cd88f9b6fcd7a5e03cd846a11ab
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-07-18 23:55:35 +0300

    добавлено и разобрана возможность назначения групп в админке + права пользователей

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,14 +1,15 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
+
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-from women.forms import AddPostForm, UploadFileForm
-from women.models import Women, TagPost, UploadFiles
-from women.utils import DataMixin
+from .forms import AddPostForm
+from .models import Women, TagPost
+from .utils import DataMixin
 
 
 class WomenHome(DataMixin, ListView):
@@ -21,52 +22,16 @@ class WomenHome(DataMixin, ListView):
         return Women.published.all().select_related('cat')
 
 
-class AddPage(LoginRequiredMixin, DataMixin, CreateView):
-    form_class = AddPostForm
-    template_name = 'women/addpage.html'
-    title_page = 'Добавление статьи'
-    # login_url = '/admin'
+@login_required
+def about(request):
+    contact_list = Women.published.all()
+    paginator = Paginator(contact_list, 3)
 
-    def form_valid(self, form):
-        w = form.save(commit=False)
-        w.author = self.request.user
-        return super().form_valid(form)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-
-class UpdatePage(DataMixin, UpdateView):
-    model = Women
-    fields = ['title', 'content', 'photo', 'is_published', 'cat']
-    template_name = 'women/addpage.html'
-    success_url = reverse_lazy('home')
-    title_page = 'Редактирование статьи'
-
-
-class WomenCategory(DataMixin, ListView):
-    template_name = 'women/index.html'
-    context_object_name = 'posts'
-    allow_empty = False
-
-    def get_queryset(self):
-        return Women.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        cat = context['posts'][0].cat
-        return self.get_mixin_context(context, title='Категория - ' + cat.name, cat_selected=cat.id)
-
-
-class TagPostList(DataMixin, ListView):
-    template_name = 'women/index.html'
-    context_object_name = 'posts'
-    allow_empty = False
-
-    def get_context_data(self, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        tag = TagPost.objects.get(slug=self.kwargs['tag_slug'])
-        return self.get_mixin_context(context, title='Тег: ' + tag.tag)
-
-    def get_queryset(self):
-        return Women.published.filter(tags__slug=self.kwargs['tag_slug']).select_related('cat')
+    return render(request, 'women/about.html',
+                  {'title': 'О сайте', 'page_obj': page_obj})
 
 
 class ShowPost(DataMixin, DetailView):
@@ -82,17 +47,28 @@ class ShowPost(DataMixin, DetailView):
         return get_object_or_404(Women.published, slug=self.kwargs[self.slug_url_kwarg])
 
 
-@login_required(login_url='users:login')
-def about(request):
-    contact_list = Women.published.all()
-    paginator = Paginator(contact_list, 3)
+class AddPage(PermissionRequiredMixin, LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    title_page = 'Добавление статьи'
+    permission_required = 'women.add_women'  # <приложение>.<действие>_<таблица>
 
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    def form_valid(self, form):
+        w = form.save(commit=False)
+        w.author = self.request.user
+        return super().form_valid(form)
 
-    return render(request, 'women/about.html', {'title': 'О сайте', 'page_obj': page_obj})
+
+class UpdatePage(PermissionRequiredMixin, DataMixin, UpdateView):
+    model = Women
+    fields = ['title', 'content', 'photo', 'is_published', 'cat']
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+    title_page = 'Редактирование статьи'
+    permission_required = 'women.change_women'
 
 
+@permission_required(perm='women.add_women', raise_exception=True)
 def contact(request):
     return HttpResponse("Обратная связь")
 
@@ -101,5 +77,36 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
+class WomenCategory(DataMixin, ListView):
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Women.published.filter(cat__slug=self.kwargs['cat_slug']).select_related("cat")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat = context['posts'][0].cat
+        return self.get_mixin_context(context,
+                                      title='Категория - ' + cat.name,
+                                      cat_selected=cat.pk,
+                                      )
+
+
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+
+class TagPostList(DataMixin, ListView):
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag = TagPost.objects.get(slug=self.kwargs['tag_slug'])
+        return self.get_mixin_context(context, title='Тег: ' + tag.tag)
+
+    def get_queryset(self):
+        return Women.published.filter(tags__slug=self.kwargs['tag_slug']).select_related('cat')
